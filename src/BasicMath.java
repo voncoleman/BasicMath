06cc8ec082d7bd9d28a2414e875d83c4c7b53e60
Von Coleman <von.coleman@yahoo.com>
2023-10-12 21:44:49 -0500

    first commit

---
+++ src/BasicMath.java
@@ -0,0 +1,9 @@
+public class BasicMath{ //You named the class BasicMath. Public class BasicMath.
+    public static void main(String[] args){ //Main. Starting point for the program. Be brilliant on the basics!
+        System.out.println( 125 + 24); //Don't comment yourself to death. Your getting good practice using Operators
+        System.out.println(125 - 24);   //You know System.out.println is your friend. Don't stop giving it props for displaying your work.
+        System.out.println( 125 * 24);
+        System.out.println(125 / 24);
+        System.out.println( 125 % 24);
+    }
+}
